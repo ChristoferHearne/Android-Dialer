6d1dc3a71ea4028136232a7f22e4009397203ee6
Christofer Hearne <chhe1903@student.miun.se>
2021-11-13 14:28:41 +0100

    Added activities and implemented functionality for all buttons.

--- app/src/main/java/se/miun/chhe1903/dt031g/dialer/MainActivity.java
+++ app/src/main/java/se/miun/chhe1903/dt031g/dialer/MainActivity.java
@@ -2,9 +2,12 @@ package se.miun.chhe1903.dt031g.dialer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-import se.miun.chhe1903.dt031g.dialer.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,4 +16,34 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void goToDial(View view){
+        Intent dialIntent = new Intent(this, DialActivity.class);
+        startActivity(dialIntent);
+    }
+    public void goToCallList(View view){
+        Intent callListIntent = new Intent(this, CallListActivity.class);
+        startActivity(callListIntent);
+    }
+    public void goToSettings(View view){
+        Intent settingsIntent = new Intent(this, SettingsActivity.class);
+        startActivity(settingsIntent);
+   }
+   public void goToMap(View view){
+        Intent mapsIntent = new Intent(this, MapsActivity.class);
+        startActivity(mapsIntent);
+   }
+   public void showAboutDialog(View view){
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+        dialogBuilder.setTitle("About")
+                .setMessage("This app is supposed to mimic the keypad on a phone.\n\n" +
+                        "This app will consist of:\n\n" +
+                        "\u2022 Enter numbers to dial\n" +
+                        "\u2022 See previously dialed numbers\n" +
+                        "\u2022 Change the keypad settings\n" +
+                        "\u2022 Show on a map where previously calls are dialed from\n"
+                )
+                .setPositiveButton("OK", null);
+        Dialog dialog = dialogBuilder.create();
+        dialog.show();
+   }
 }
