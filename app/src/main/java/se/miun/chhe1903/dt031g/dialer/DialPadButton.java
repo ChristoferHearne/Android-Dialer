8fd47e453de7b36ac12e49e1a1480956f5c0d637
Christofer Hearne <chhe1903@student.miun.se>
2021-11-19 23:55:25 +0100

    onTouchListener is now added to each button that is created. Changed background color in dial_pad_button.xml.

--- app/src/main/java/se/miun/chhe1903/dt031g/dialer/DialPadButton.java
+++ app/src/main/java/se/miun/chhe1903/dt031g/dialer/DialPadButton.java
@@ -1,17 +1,18 @@
 package se.miun.chhe1903.dt031g.dialer;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
-import android.view.View;
 import android.widget.TextView;
 
 import androidx.constraintlayout.widget.ConstraintLayout;
 
-
+@SuppressLint("ClickableViewAccessibility")
 public class DialPadButton extends ConstraintLayout {
     // Constructor with context and AttributeSet
     public DialPadButton(Context context, AttributeSet attrs) {
@@ -25,6 +26,21 @@ public class DialPadButton extends ConstraintLayout {
             this.setMessage(message.toString());
             this.setTitle(title.toString());
         }
+        this.setOnTouchListener((v, event) -> {
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_DOWN: {
+                    v.getBackground().setColorFilter(Color.LTGRAY, PorterDuff.Mode.SRC_ATOP);
+                    v.invalidate();
+                    return true;
+                }
+                case MotionEvent.ACTION_UP: {
+                    v.getBackground().clearColorFilter();
+                    v.invalidate();
+                    break;
+                }
+            }
+            return false;
+        });
     }
     // Constructor with context only
     public DialPadButton(Context context){
