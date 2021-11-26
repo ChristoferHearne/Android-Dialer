d88ec56c9d6eefba8857b3b08f4312fee3ec938a
Christofer Hearne <chhe1903@student.miun.se>
2021-11-26 14:48:55 +0100

    Added an onLoadCompleteListener to my SoundPlayer.java, sound only plays in debugging mode.

--- app/src/main/java/se/miun/chhe1903/dt031g/dialer/SoundPlayer.java
+++ app/src/main/java/se/miun/chhe1903/dt031g/dialer/SoundPlayer.java
@@ -2,24 +2,28 @@ package se.miun.chhe1903.dt031g.dialer;
 
 import android.content.Context;
 import android.media.SoundPool;
+import android.media.SoundPool.OnLoadCompleteListener;
+
+import androidx.loader.content.Loader;
 
 import java.lang.reflect.Field;
+import java.util.ArrayList;
 
 
 public class SoundPlayer {
     // Instance variables
     private static SoundPlayer INSTANCE = null;
     private SoundPool soundPool;
+    private int soundIDThree;
+    private boolean soundIsLoaded;
 
     // Constructor
     private SoundPlayer(Context context) throws IllegalAccessException {
         soundPool = new SoundPool.Builder()
                 .setMaxStreams(1)
                 .build();
-        Field[] soundFiles = R.raw.class.getDeclaredFields();
-        for (int i = 0; i < soundFiles.length; i++){
-            soundPool.load(context, soundFiles[i].getInt(soundFiles[i]), 1);
-        }
+        soundIDThree = soundPool.load(context, R.raw.three, 1);
+        soundPool.setOnLoadCompleteListener((soundPool, sampleId, status) -> soundIsLoaded = true);
     }
     // Singleton getInstance function
     public static SoundPlayer getInstance(Context context) throws IllegalAccessException {
@@ -30,44 +34,44 @@ public class SoundPlayer {
     }
     // Functions
     public void playSound(DialPadButton dialPadButton){
-        switch(dialPadButton.getTitle()){
-            case "0":
-                soundPool.play(R.raw.zero, 1, 1, 1, 0, 1f);
-                break;
-            case "1":
-                soundPool.play(R.raw.one, 1, 1, 1, 0, 1f);
-                break;
-            case "2":
-                soundPool.play(R.raw.two, 1, 1, 1, 0, 1f);
-                break;
-            case "3":
-                soundPool.play(R.raw.three, 1, 1, 1, 0, 1f);
-                break;
-            case "4":
-                soundPool.play(R.raw.four, 1, 1, 1, 0, 1f);
-                break;
-            case "5":
-                soundPool.play(R.raw.five, 1, 1, 1, 0, 1f);
-                break;
-            case "6":
-                soundPool.play(R.raw.six, 1, 1, 1, 0, 1f);
-                break;
-            case "7":
-                soundPool.play(R.raw.seven, 1, 1, 1, 0, 1f);
-                break;
-            case "8":
-                soundPool.play(R.raw.eight, 1, 1, 1, 0, 1f);
-                break;
-            case "9":
-                soundPool.play(R.raw.nine, 1, 1, 1, 0, 1f);
-                break;
-            case "*":
-                soundPool.play(R.raw.star, 1, 1, 1, 0, 1f);
-                break;
-            case "#":
-                soundPool.play(R.raw.pound, 1, 1, 1, 0, 1f);
-                break;
-        }
+            switch(dialPadButton.getTitle()){
+                case "0":
+                    soundPool.play(R.raw.zero, 50, 50, 1, 0, 1f);
+                    break;
+                case "1":
+                    soundPool.play(R.raw.one, 1, 1, 1, 0, 1f);
+                    break;
+                case "2":
+                    soundPool.play(R.raw.two, 1, 1, 1, 0, 1f);
+                    break;
+                case "3":
+                    soundPool.play(soundIDThree, 50, 50, 1, 0, 1f);
+                    break;
+                case "4":
+                    soundPool.play(R.raw.four, 1, 1, 1, 0, 1f);
+                    break;
+                case "5":
+                    soundPool.play(R.raw.five, 1, 1, 1, 0, 1f);
+                    break;
+                case "6":
+                    soundPool.play(R.raw.six, 1, 1, 1, 0, 1f);
+                    break;
+                case "7":
+                    soundPool.play(R.raw.seven, 1, 1, 1, 0, 1f);
+                    break;
+                case "8":
+                    soundPool.play(R.raw.eight, 1, 1, 1, 0, 1f);
+                    break;
+                case "9":
+                    soundPool.play(R.raw.nine, 1, 1, 1, 0, 1f);
+                    break;
+                case "*":
+                    soundPool.play(R.raw.star, 1, 1, 1, 0, 1f);
+                    break;
+                case "#":
+                    soundPool.play(R.raw.pound, 1, 1, 1, 0, 1f);
+                    break;
+            }
     }
     public void destroy(){
         soundPool.release();
