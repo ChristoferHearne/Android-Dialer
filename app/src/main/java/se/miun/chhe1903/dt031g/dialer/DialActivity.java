917a5711422e2d9a32386a95950c5b08d36013c6
Christofer Hearne <chhe1903@student.miun.se>
2021-11-28 21:58:20 +0100

    Removed initialization of a button from DialActivity.java

--- app/src/main/java/se/miun/chhe1903/dt031g/dialer/DialActivity.java
+++ app/src/main/java/se/miun/chhe1903/dt031g/dialer/DialActivity.java
@@ -10,10 +10,5 @@ public class DialActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dial);
-
-        DialPadButton myButton = new DialPadButton(this);
-        myButton.setTitle("1-this-is-to-long");
-        myButton.setMessage("ABC-this-is-to-long");
-        setContentView(myButton);
     }
 }
