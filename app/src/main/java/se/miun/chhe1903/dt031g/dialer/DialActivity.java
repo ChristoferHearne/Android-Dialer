e0185c7c9822a30ee1e60f04b5b6a556a0dfbea1
Christofer Hearne <chhe1903@student.miun.se>
2021-12-18 00:46:49 +0100

    Marker is added to the Maps-view. Needs subtitle with timestamp and bigger marker icon.

--- app/src/main/java/se/miun/chhe1903/dt031g/dialer/DialActivity.java
+++ app/src/main/java/se/miun/chhe1903/dt031g/dialer/DialActivity.java
@@ -82,9 +82,6 @@ public class DialActivity extends AppCompatActivity {
             }
             else{
                 requestPermissions();
-                if (callPermissionGranted){
-                    performCallOperation(context);
-                }
             }
         });
     }
--- app/src/main/java/se/miun/chhe1903/dt031g/dialer/MapsActivity.java
+++ app/src/main/java/se/miun/chhe1903/dt031g/dialer/MapsActivity.java
@@ -1,13 +1,19 @@
 package se.miun.chhe1903.dt031g.dialer;
 
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -69,9 +75,18 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             if (hasLocationData){
                 mMap.addMarker(new MarkerOptions()
                         .position(new LatLng(storedCall.getLatitude(), storedCall.getLongitude()))
-                        .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_baseline_call_24)))
+                        .icon(bitmapDescriptorFromVector(getApplicationContext(), R.drawable.ic_baseline_call_24)))
                         .setTitle(storedCall.getNumber());
             }
         }
+    }
+
+    private BitmapDescriptor bitmapDescriptorFromVector(Context context, int vectorResId) {
+        Drawable vectorDrawable = ContextCompat.getDrawable(context, vectorResId);
+        vectorDrawable.setBounds(0, 0, vectorDrawable.getIntrinsicWidth(), vectorDrawable.getIntrinsicHeight());
+        Bitmap bitmap = Bitmap.createBitmap(vectorDrawable.getIntrinsicWidth(), vectorDrawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        vectorDrawable.draw(canvas);
+        return BitmapDescriptorFactory.fromBitmap(bitmap);
     }
 }
